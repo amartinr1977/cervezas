d0dbc1086cf4c148f53f2211af78c48ec54a69fd
Arturo Martín Romero <amartinromero@gmail.com>
2018-09-07 12:32:27 +0200

    ok ...

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-const cervezas = require('./cervezas.json');
+const cervezas = require('./cervezas.json')
+const uniqueRandomArray = require('unique-random-array')
+
+const prueba = function () {
+  return 'Es es mi función de prueba'
+}
 module.exports = {
-  todas: cervezas
-};
+  // Todas devuelve un json con todas
+  todas: cervezas,
+  // uniqueRandowArray devuelve a una función:
+  // Para probarlo desde una consola de node:
+  // $ node
+  // > const una = require('./src/index')
+  // > console.log(una.alazar())
+  alazar: uniqueRandomArray(cervezas)
+}
