a3135b3e348bca9b73bf3491097ac05ac55e2c9b
Arturo Martín Romero <amartinromero@gmail.com>
2018-09-07 13:18:52 +0200

    v.1.1.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const cervezas = require('./cervezas.json')
 const uniqueRandomArray = require('unique-random-array')
 
+const _ = require('lodash')
+
 const prueba = function () {
   return 'Es es mi función de prueba'
 }
 module.exports = {
   // Todas devuelve un json con todas
-  todas: cervezas,
+  todas: _.sortBy(cervezas, ['nombre']),
   // uniqueRandowArray devuelve a una función:
   // Para probarlo desde una consola de node:
   // $ node
